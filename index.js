f77647c5ec3e89c5feae914dfa0ee296ed535b25
Pseudocoder Kim <digression99@gmail.com>
2019-10-09 15:43:17 +0900

    Test nested path parameter

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ app.get('/', (req, res) => {
   res.send('hi');
 });
 
+// app.get('/path-parameter;*', (req, res) => {
+//   console.log('url : ', req.url);
+//   res.send('GET /path-parameter;*');
+// });
+
+app.get('/path-parameter', (req, res) => {
+  console.log('url : ', req.url);
+  res.send('GET /path-parameter');
+});
+
+const router = express.Router();
+
+router.get('/nested-path;*', (req, res) => {
+  console.log('GET /nested-path;*');
+  console.log('url : ', req.url);
+  res.send('GET /nested-path;*');
+});
+
+app.use('/path-parameter2;*', router);
+
 app.listen(app.get('port'), () => {
   console.log('app listening on, ', app.get('port'));
 });
